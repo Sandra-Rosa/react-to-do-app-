5f813d23808b24b79051a1230508c46dd5d85f20
Sandra-Rosa <sandraantony2002@gmail.com>
2022-08-02 20:26:09 +0530

    Finished

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import {useState} from 'react'
 function App() {
   const [toDos,setToDos] = useState([])
   const [toDo,setToDo] = useState('')
+  const deleteTask = (value)=> {
+
+    var ask = window.confirm("Do you want to delete the task?");
+    if (ask) {
+      const test = [...toDos];
+        test.splice(value, 1);
+
+        setToDos(test)
+    }else {
+      console.log("Don't delete");
+    }
+
+   
+  }
   return (
     <div className="div1">
       <span className="nav">Sandra Rosa Antony</span>
@@ -17,16 +31,28 @@ function App() {
             Let’s be productive...
           </span>
           <div className="app">
-            <input type="text" placeholder="Add item..." className="type" />
-            <i className="fas fa-plus"></i>
+            <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="Add item..." className="type" />
+            <i onClick={()=>setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
           </div>
-          <div className="app2">
-            <input type="checkbox" name="" id="check" />
+          { toDos.map((value)=>{
+            return (
+            <div className="app2">
+            <input onChange={
+              (e)=>{
+                  setToDos(toDos.filter(obj=>{
+                    if(obj.id===value.id){
+                       obj.status=e.target.checked
+                    }
+                    return obj
+                  }))
+              }
+            } value={value.status} type="checkbox" name="" id="check" />
             <span className='type2'>
-              React Tutorial
+              {value.text}
             </span>
-            <i className="fas fa-times"></i>
-          </div>
+            <i className="fas fa-times" onClick={()=>deleteTask(value)}></i>
+          </div>)
+          })}
         </div>
       </div>
     </div>
