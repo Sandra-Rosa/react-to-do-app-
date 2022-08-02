41bfba3d233d43328dcbefee9389061962ddfc62
Sandra-Rosa <sandraantony2002@gmail.com>
2022-08-02 19:31:39 +0530

    frontend with css completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import logo from './girl-todo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div1">
+      <span className="nav">Sandra Rosa Antony</span>
+      <div className="main">
+        <img src={logo} alt="Logo" className="girl" />
+        <div className="content">
+          <span className="Heading">
+            To-Do App
+          </span>
+          <span className="paragraph">
+            Let’s be productive...
+          </span>
+          <div className="app">
+            <input type="text" placeholder="Add item..." className="type" />
+            <i className="fas fa-plus"></i>
+          </div>
+          <div className="app2">
+            <input type="checkbox" name="" id="check" />
+            <span className='type2'>
+              React Tutorial
+            </span>
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
